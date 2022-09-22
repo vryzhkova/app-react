eff83b2169b1f7bf81dd3b4973b1d4ee056c63bf
Viktoryia Ryzhkova <viktiria87654321@gmail.com>
2022-09-22 16:52:16 +0400

    added hello world

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import './index.css';
+import App, { Header } from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Header />
   </React.StrictMode>
 );
-
